c148d11d6e80bed0d886b26e09d539e1c7bc8012
Skandar Munir Ahmed <sikandernoori@gmail.com>
2024-06-14 15:05:35 +0300

    implement retry on timeout

--- README.md
+++ README.md
@@ -21,6 +21,7 @@
     issuer-id: ${{ vars.APPSTORE_ISSUER_ID }}
     api-key-id: ${{ vars.APPSTORE_API_KEY_ID }}
     api-private-key: ${{ secrets.APPSTORE_API_PRIVATE_KEY }}
+    retry-attempts-on-timeout: 2
 ```
 
 ## Additional Arguments
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import {getInput, setOutput, setFailed} from '@actions/core'
+import {getInput, setOutput, setFailed, warning} from '@actions/core'
 import {platform} from 'os'
 import {installPrivateKey, uploadApp, deleteAllPrivateKeys} from './altool'
-
+import {retry} from 'ts-retry-promise'
 import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
@@ -15,6 +15,12 @@ async function run(): Promise<void> {
     const apiPrivateKey: string = getInput('api-private-key')
     const appPath: string = getInput('app-path')
     const appType: string = getInput('app-type')
+    const retryAttempts: number = parseInt(
+      getInput('retry-attempts-on-timeout')
+    )
+    const retryWaitSeconds: number = parseInt(
+      getInput('retry_wait_seconds')
+    )
 
     let output = ''
     const options: ExecOptions = {}
@@ -25,9 +31,34 @@ async function run(): Promise<void> {
     }
 
     await installPrivateKey(apiKeyId, apiPrivateKey)
-    await uploadApp(appPath, appType, apiKeyId, issuerId, options)
-    await deleteAllPrivateKeys()
+    const uploadWithRetry = async (): Promise<void> => {
+      try {
+        await uploadApp(appPath, appType, apiKeyId, issuerId, options)
+      } catch (e) {
+        if (output.includes('The request timed out')) {
+          throw Error('timeout')
+        }
 
+        throw e
+      }
+    }
+
+    try {
+      await retry(uploadWithRetry, {
+        retries: retryAttempts,
+        delay: retryWaitSeconds * 1000,
+        retryIf(error) {
+          return error.message === 'timeout'
+        }
+      })
+    } catch (error: unknown | Error) {
+      warning(
+        `Upload failed after ${retryAttempts} attempts: ${(error as Error).message || 'An unknown error occurred.'}`
+      )
+      throw error
+    }
+
+    await deleteAllPrivateKeys()
     setOutput('altool-response', output)
   } catch (error: unknown | Error) {
     setFailed((error as Error).message || 'An unknown error occurred.')
